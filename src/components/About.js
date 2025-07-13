0c45c93a7c5fc884b61ae45334a8bdac912aae44
Wambui Kibathi <zeldabobo02@gmail.com>
2025-07-13 12:35:24 +0300

    Changes within the About.js App.js Links.js files

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 import React from "react";
+import {image} from "../data/user";
+import Links from "./Links";
 
-function About() {
+function About({ bio, github, linkedin}) {
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <h2>About Me</h2> 
+      {
+        bio && <p>{bio}</p>
+      }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={github} linkedin={linkedin} /> {/* Render links */}
     </div>
   );
 }
